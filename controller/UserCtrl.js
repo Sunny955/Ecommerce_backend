8cc818a75076357faddb887c56d98da635d1c244
Sunny Ranjan <sunnyranjan@Sunnys-MacBook-Air.local>
2023-10-09 00:28:33 +0530

    Added route for multiple and single user

--- controller/UserCtrl.js
+++ controller/UserCtrl.js
@@ -1,6 +1,11 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/UserModel");
 const asyncHandler = require("express-async-handler");
+const mongoose = require('mongoose');
+
+const validateMongoDbId = (id) => {
+    return mongoose.Types.ObjectId.isValid(id);
+};
 
 const createUser = asyncHandler(async(req,res) => {
     const email = req.body.email;
@@ -46,4 +51,61 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {createUser,loginUser};
+// get all users
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+      const limit = parseInt(req.query.limit) || 10; // Default limit is 10
+      const skip = parseInt(req.query.skip) || 0; // Default skip is 0
+  
+      const users = await User.find()
+                            .limit(limit)
+                            .skip(skip);
+  
+      res.status(200).json({
+        success: true,
+        count: users.length,
+        data: users
+      });
+    } catch (error) {
+      res.status(500); // Internal Server Error
+      throw new Error("Error retrieving users: " + error.message);
+    }
+});
+
+// to get a single user
+
+const getaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    // Validate MongoDB ID
+    if (!validateMongoDbId(id)) {
+        return res.status(400).json({
+            success: false,
+            message: 'Provided ID is not a valid MongoDB ID.'
+        });
+    }
+
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found with the provided ID.'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error retrieving user: ' + error.message
+        });
+    }
+});
+
+  
+
+module.exports = {createUser,loginUser,getAllUsers,getaUser};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { createUser, loginUser } = require("../controller/UserCtrl");
+const { createUser, loginUser, getAllUsers, getaUser } = require("../controller/UserCtrl");
 const router = express.Router();
 
 router.post('/register',createUser);
 router.post('/login',loginUser);
+router.get("/all-users",getAllUsers);
+router.get("/:id",getaUser);
 
 module.exports = router;
